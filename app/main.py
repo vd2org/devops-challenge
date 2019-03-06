6eaf19bf1138fdcbc78f78d6f45b3af9858d090f
vd <vd@>
2019-03-06 03:23:53 +0200

    Checking connection on startup.

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app import app
 import os
 
 import boto3
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, EndpointConnectionError
 
 import health
 import secret
@@ -21,21 +21,27 @@ try:
                                 region_name=os.environ['AWS_REGION'])
 
     # setup dynamodb connection
-    # dynamodb_client = boto3.client('dynamodb', endpoint_url=os.environ.get('DYNAMO_ENDPOINT'))
     dynamo = boto3.resource('dynamodb', endpoint_url=os.environ.get('DYNAMO_ENDPOINT'))
     table = dynamo.Table('devops-challenge')
+
+    # checking connection
+    is_table_existing = table.table_status in ("CREATING", "UPDATING", "DELETING", "ACTIVE")
 
 except KeyError as e:
     # missing configuration environment
     raise RuntimeError('Unable to get configuration env var `%s`' % str(e)) from e
+
+except EndpointConnectionError as e:
+    # connection error
+    raise RuntimeError('Dynamo is db not available.') from e
+
 except ClientError as e:
     if e.response['Error']['Code'] == 'ResourceNotFoundException':
         # required table not found in dynamodb
         raise RuntimeError('Required table does not exist.') from e
-    if e.response['Error']['Code'] == 'EndpointConnectionError':
-        # connection error
-        raise RuntimeError('Dynamo db not avaliable.') from e
-
+    if e.response['Error']['Code'] in ('UnrecognizedClientException', 'AccessDeniedException'):
+        # required table not found in dynamodb
+        raise RuntimeError('Dynamo error. Probably your access token is invalid.') from e
     # reraise other exceptions
     raise
 
